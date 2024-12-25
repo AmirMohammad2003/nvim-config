27708229a3ccf87e361b404720b665d78e4af02d
AmirMohammad Ghadimi <amirmohammadghadimi8@gmail.com>
2024-12-25 22:52:02 +0330

    Switch over to lualine from mini

--- lua/mamad/plugins/mini.lua
+++ lua/mamad/plugins/mini.lua
@@ -5,15 +5,20 @@ return {
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
 
-		local statusline = require("mini.statusline")
-		statusline.setup({ use_icons = vim.g.have_nerd_font })
+		-- local statusline = require("mini.statusline")
+		-- statusline.setup({ use_icons = vim.g.have_nerd_font })
 
 		---@diagnostic disable-next-line: duplicate-set-field
-		statusline.section_location = function()
-			return "%2l:%-2v"
-		end
-
-		require("mini.tabline").setup()
+		-- statusline.section_location = function()
+		-- 	return "%2l:%-2v"
+		-- end
+		--
+		-- require("mini.tabline").setup({
+		-- 	show_icons = true,
+		-- 	format = nil,
+		-- 	set_vim_settings = false,
+		-- 	tabpage_section = "left",
+		-- })
 		vim.keymap.set("n", "L", vim.cmd.bn, { noremap = true, silent = true })
 		vim.keymap.set("n", "H", vim.cmd.bp, { noremap = true, silent = true })
 
--- lua/mamad/plugins/oil.lua
+++ lua/mamad/plugins/oil.lua
@@ -1,7 +1,15 @@
 return {
 	"stevearc/oil.nvim",
 	opts = {},
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = {
+		{
+			"echasnovski/mini.icons",
+			opts = {},
+			config = function()
+				require("mini.icons").setup({})
+			end,
+		},
+	},
 	config = function()
 		require("oil").setup()
 		vim.keymap.set("n", "<Leader>e", function()
