9ee3425a339893f91633cf7693d20b75ec9db555
AmirMohammad Ghadimi <amirmohammadghadimi8@gmail.com>
2025-04-19 11:19:59 +0330

    Clean up mini plugins

--- lua/mamad/plugins/mini.lua
+++ lua/mamad/plugins/mini.lua
@@ -3,23 +3,6 @@ return {
 	config = function()
 		--  https://github.com/echasnovski/mini.nvim
 		require("mini.ai").setup({ n_lines = 500 })
-		-- require("mini.surround").setup()
-
-		-- local statusline = require("mini.statusline")
-		-- statusline.setup({ use_icons = vim.g.have_nerd_font })
-
-		---@diagnostic disable-next-line: duplicate-set-field
-		-- statusline.section_location = function()
-		-- 	return "%2l:%-2v"
-		-- end
-		--
-		-- require("mini.tabline").setup({
-		-- 	show_icons = true,
-		-- 	format = nil,
-		-- 	set_vim_settings = false,
-		-- 	tabpage_section = "left",
-		-- })
-
 		require("mini.cursorword").setup()
 	end,
 }
